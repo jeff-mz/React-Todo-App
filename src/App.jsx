a3de99b8afa94ba9dcac1be5c1a7dbf121d20008
jeff-mz <jafarmirzapoor11@gmail.com>
2024-03-07 17:11:48 +0330

    UI improved and new features added.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <main
       id="main"
-      className=" w-full h-full py-12 flex flex-col items-center justify-center  font-display bg-[#2b2d42] min-h-screen"
+      className=" w-full h-full flex flex-col items-center font-display bg-[#2b2d42] min-h-screen"
     >
       <TodoCard />
-      <p className="text-[16px] font-bold text-stone-100 mt-[16px]">
+      <p className="text-[16px] font-bold text-stone-100 mt-[16px] xl:text-2xl xl:my-12">
         Made by Jafar Mirzapoor with 💗
       </p>
     </main>
--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,73 +1,95 @@
 import React, { useState } from "react";
-import { MdDeleteForever } from "react-icons/md";
+import { MdOutlineEdit, MdDeleteForever, MdOutlineDone } from "react-icons/md";
+
 function TodoCard() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-  const changeHandler = (el) => {
-    setTodo(el.target.value);
+  const handleInputChange = (event) => {
+    setTaskInput(event.target.value);
   };
 
-  const submitHandler = () => {
-    todo.length > 0 &&
-      setTodos((pervTodos) => [
-        ...pervTodos,
-        { task: todo, id: Math.floor(Math.random() * 1000) },
+  const handleTaskSubmission = () => {
+    if (taskInput.length > 0) {
+      setTasks((prevTasks) => [
+        ...prevTasks,
+        {
+          description: taskInput,
+          id: Math.floor(Math.random() * 1000),
+          done: false,
+        },
       ]);
-    setTodo("");
+      setTaskInput("");
+    }
   };
 
-  const deleteHandler = (id) => {
-    let filteredTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(filteredTodos);
+  const handleTaskDeletion = (taskId) => {
+    const filteredTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(filteredTasks);
+  };
+
+  const handleTaskCompletion = (taskId) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, done: !task.done } : task
+      )
+    );
   };
 
   return (
-    <div className="w-[90%]  p-2 rounded- flex flex-col items-center ">
-      <h1 className="text-4xl text-white font-bold my-8 text-center md:text-6xl">
+    <div className="w-full rounded- flex flex-col items-center">
+      <h1 className="text-4xl text-white font-bold my-8 text-center md:text-6xl 2xl:text-8xl 2xl:my-16">
         Get Things Done!
       </h1>
       {/* input start */}
       <div className="w-4/5 mb-8 mx-auto">
         <input
           type="text"
-          value={todo}
-          onChange={changeHandler}
-          placeholder="What is next todo?"
+          value={taskInput}
+          onChange={handleInputChange}
+          placeholder="Create a new task..."
           style={{ borderRadius: "6px 0px 0px 6px" }}
-          className="w-3/5 p-2 text-[16px] border-none outline-none lg:w-4/5 md:text-2xl md:p-4"
+          className="w-3/5 px-2 py-4 xl:py-8 text-[16px] border-none outline-none lg:w-4/5 md:text-2xl md:p-4 xl:text-4xl 2xl:text-6xl 2xl:py-7"
         />
         <button
           type="submit"
-          onClick={submitHandler}
+          onClick={handleTaskSubmission}
           style={{ borderRadius: "0px 6px 6px 0px" }}
-          className="w-2/5 text-[16px] bg-[#ffaf3e] p-2 text-[#1e221e] font-medium lg:w-1/5 md:text-2xl md:p-4"
+          className="w-2/5 px-2 py-4 xl:py-8 text-[16px] bg-[#ffaf3e]  text-[#1e221e] font-medium lg:w-1/5 md:text-2xl md:p-4 xl:text-4xl 2xl:text-6xl"
         >
           Add Task
         </button>
       </div>
       {/* input end  */}
-      {/* Todos List  */}
-      {todos.map((task) => {
-        return (
-          <div
-            key={task.id}
-            className="w-4/5 bg-[#404255] p-2 flex items-center my-4 rounded-md justify-between md:p-4"
-          >
+
+      {/* Tasks List  */}
+      <ul className="w-4/5 bg-[#3f405f] min-h-52 list-disc rounded-sm pb-4 md:pb-10">
+        <p className="text-center text-slate-300 font-bold py-4 text-[18px] md:text-3xl xl:text-5xl 2xl:text-7xl md:py-6 xl:py-8 2xl:py-12">
+          Your task list
+        </p>
+        {tasks.map((task) => (
+          <li className="w-[90%] list-decimal flex items-center py-4 px-4 m-auto rounded-lg justify-between bg-[#57598d]">
             <p
-              className={`text-[18px] text-white font-medium md:text-4xl md:py-2`}
+              className={`text-[18px] ${
+                task.done ? "line-through" : ""
+              } text-white font-medium md:py-2 md:text-2xl md:p-4 xl:text-4xl 2xl:text-6xl`}
             >
-              {task.task}
+              {task.description}
             </p>
             <div>
               <MdDeleteForever
-                onClick={() => deleteHandler(task.id)}
-                className="inline text-[#d0342c] md:text-4xl md:mx4"
+                onClick={() => handleTaskDeletion(task.id)}
+                className="inline text-[#d0342c] mx-2 text-[18px] md:mx-4 xl:mx-10 md:text-3xl xl:text-5xl 2xl:text-6xl"
+              />
+              <MdOutlineDone
+                onClick={() => handleTaskCompletion(task.id)}
+                className="inline text-[#2cd04d] text-[18px] md:text-3xl xl:text-5xl 2xl:text-6xl"
               />
             </div>
-          </div>
-        );
-      })}
+          </li>
+        ))}
+      </ul>
+      {/* End of Task list  */}
     </div>
   );
 }
